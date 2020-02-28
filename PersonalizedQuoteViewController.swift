5651ddff73afbee71b02fd32d7537247a086fd1e
berm <berm@berm.nl>
2020-02-28 10:50:32 -0800

    test

--- PersonalizedQuoteViewController.swift
+++ PersonalizedQuoteViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class PersonalizedQuoteViewController: UIViewController {
     let defaults = UserDefaults.standard
-
+    
     @IBOutlet weak var QuoteLabel: UILabel!
     @IBOutlet weak var NameTextField: UITextField!
     override func viewDidLoad() {
@@ -30,20 +30,34 @@ class PersonalizedQuoteViewController: UIViewController {
         let link = "https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q=" + name
         let encodedStr = link.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
         let url = URL(string:encodedStr!)
+        
+        guard url == url else {
+            return
+        }
+        
         let task = URLSession.shared.dataTask(with: url!) { receivedData, response, error in
+            guard error == nil else {
+                return
+            }
+            guard let retrievedData = receivedData else {
+                return
+            }
             do {
-                if let welcome = try? JSONDecoder().decode(QuoteModel.self, from: (receivedData!))
-                {
-                    if let quote = welcome.message {
-                        self.WriteToMainThread(text: "\(quote)", label: self.QuoteLabel)
-                    } else {
-                        self.WriteToMainThread(text: "No message recieved...", label: self.QuoteLabel)
-                    }
+                let decoder = JSONDecoder()
+                let welcome = try decoder.decode(QuoteModel.self, from: retrievedData)
+                if let quote = welcome.message {
+                    self.WriteToMainThread(text: "\(quote)", label: self.QuoteLabel)
+                } else {
+                    self.WriteToMainThread(text: "No message recieved...", label: self.QuoteLabel)
                 }
+                
+            } catch {
+                
             }
         }
         task.resume()
+        
     }
-}	
+}
 
 
